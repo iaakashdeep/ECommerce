9745b1521dbc78ed81b18727296c245b2f1a291e
iaakashdeep <deep.0812@gmail.com>
2024-08-13 22:50:45 +0530

    Committed changes for Identity

--- ECommerce.DataAccess/Data/ApplicationDbContext.cs
+++ ECommerce.DataAccess/Data/ApplicationDbContext.cs
@@ -1,23 +1,32 @@
 ﻿using ECommerce.Models.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
         }
 
-        public DbSet<Category> Categories { get; set; }  //<Category> here we define the entity whatever we want to create a table for in DB
+        public DbSet<Category> Categories { get; set; }  
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<ApplicationUsers> ApplicationUsers { get; set; }
         
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Seed();
-            modelBuilder.SeedProduct(); //Extention methods created for modelBuilder
+            modelBuilder.SeedProduct(); 
         }
 
     }
-} 
+}
+
+//IdentityDbContext this class is used instead of DBCOntext because this will provide Identity secutity features
+
+//base.OnModelCreating(modelBuilder); we have to write this line otherwise primary key error will come
